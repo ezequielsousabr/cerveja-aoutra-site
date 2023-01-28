043ef2a970d30d88723d053b4a4828b80ffb7483
Ezequiel L Sousa <ezequiel.sousa@yahoo.com>
2023-01-28 17:36:59 -0300

    Criação do menu dropdown

--- js/menu-dropdown.js
+++ js/menu-dropdown.js
@@ -1,1 +1,40 @@
-const dropdown = document.querySelector('.dropdown', '.dropdown-suspenso');
+var dropdown, dropdownHidden;
+
+dropdown = document.querySelector('.dropdown', '.dropdown-suspenso');
+
+console.log(dropdown);
+
+
+document.addEventListener('click', (e) => {
+
+    const isDropdown = e.target.classList.contains('navbar-toogle');
+
+    console.log(isDropdown);
+
+    if(!isDropdown && e.target.closest('.dropdown-suspenso') !== null ) return;
+
+    let currentSubmenuBtn;
+
+    if(isDropdown){
+
+        const item = e.target.closest('.dropdown-suspenso')
+
+        currentSubmenuBtn = item.querySelector('.dropdown-suspenso');
+
+        console.log(currenSubmenuBtn);
+
+        currentSubmenuBtn.classList.toggle('show');
+
+        document.querySelector('.dropdown-suspenso.show').forEach(isDropdown => {
+
+            if(isDropdown === currentSubmenuBtn){
+
+                currentSubmenuBtn.classList.remove('show');
+
+            }
+
+        });
+
+    };
+
+})
